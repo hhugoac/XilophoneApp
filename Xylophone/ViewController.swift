04fc294458d802d5122261b3b9e929d4df9529af
Hugo <hhugoac@gmail.com>
2023-05-02 17:49:09 -0600

    Adding the challenge files to the proejct

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
-    
+    @IBAction func keyPress(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            UIView.animate(withDuration: 0.3) {
+                sender.alpha = 1.0
+            }
+        }
 
+        playSound(sound: sender.currentTitle!)
+    }
+    
+    func playSound(sound: String) {
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
